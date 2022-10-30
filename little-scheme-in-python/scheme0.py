65ee599e79fdabfeb95f5c5e0e5585d06a66f56a
Tianyu Gu <macdavid313@gmail.com>
2022-10-30 17:25:41 +1100

    refactor

--- little-scheme-in-python/scheme0.py
+++ little-scheme-in-python/scheme0.py
@@ -17,9 +17,30 @@ from operator import (
 )
 from typing import Any, Dict, List
 
-from sexpdata import Quoted, Symbol
-from sexpdata import loads as parse_sexp
 from prompt_toolkit import PromptSession
+from sexpdata import Quoted, Symbol
+from sexpdata import dumps as write_sexp
+from sexpdata import loads as parse_sexp
+
+
+class SchemeRuntimeError(RuntimeError):
+    pass
+
+
+class SchemeUnboundVaribleError(SchemeRuntimeError):
+    def __init__(self, var: Symbol):
+        self.var = var
+
+    def __str__(self):
+        return "Unbound variable: {0}".format(self.var.value())
+
+
+class SchemeInvalidExpressionError(SchemeRuntimeError):
+    def __init__(self, expr):
+        self.expr = expr
+
+    def __str__(self):
+        return "Invalid expression: {0}".format(write_sexp(self.expr))
 
 
 class SchemeEnvironment:
@@ -29,7 +50,7 @@ class SchemeEnvironment:
     def get_var(self, var: Symbol):
         val = self.table.get(var.value())
         if not val:
-            raise RuntimeError("Unbound variable: %s" % var)
+            raise SchemeUnboundVaribleError(var)
         return val
 
     def set_var(self, var: Symbol, val: Any):
@@ -44,61 +65,48 @@ class SchemeEnvironment:
 
     @staticmethod
     def init_global():
-        global_scheme_environment = SchemeEnvironment()
+        global_env = SchemeEnvironment()
 
         # arithmetic
-        global_scheme_environment.set_var(Symbol("+"), add)
-        global_scheme_environment.set_var(Symbol("-"), sub)
-        global_scheme_environment.set_var(Symbol("*"), mul)
-        global_scheme_environment.set_var(Symbol("/"), truediv)
-        global_scheme_environment.set_var(Symbol("floor"), floordiv)
-        global_scheme_environment.set_var(Symbol("mod"), mod)
+        global_env.set_var(Symbol("+"), add)
+        global_env.set_var(Symbol("-"), sub)
+        global_env.set_var(Symbol("*"), mul)
+        global_env.set_var(Symbol("/"), truediv)
+        global_env.set_var(Symbol("floor"), floordiv)
+        global_env.set_var(Symbol("mod"), mod)
         # boolean
-        global_scheme_environment.set_var(Symbol("and"), and_)
-        global_scheme_environment.set_var(Symbol("or"), or_)
-        global_scheme_environment.set_var(Symbol("not"), not_)
+        global_env.set_var(Symbol("and"), and_)
+        global_env.set_var(Symbol("or"), or_)
+        global_env.set_var(Symbol("not"), not_)
         # comparison
-        global_scheme_environment.set_var(Symbol("="), eq)
-        global_scheme_environment.set_var(Symbol("!="), ne)
-        global_scheme_environment.set_var(Symbol("<"), lt)
-        global_scheme_environment.set_var(Symbol("<="), le)
-        global_scheme_environment.set_var(Symbol(">"), gt)
-        global_scheme_environment.set_var(Symbol(">="), ge)
+        global_env.set_var(Symbol("="), eq)
+        global_env.set_var(Symbol("!="), ne)
+        global_env.set_var(Symbol("<"), lt)
+        global_env.set_var(Symbol("<="), le)
+        global_env.set_var(Symbol(">"), gt)
+        global_env.set_var(Symbol(">="), ge)
 
-        return global_scheme_environment
+        return global_env
 
 
 def eval_scheme(x, env: SchemeEnvironment):
-
-    if isinstance(x, int) or isinstance(x, float):
-        return x
-
-    elif isinstance(x, Quoted):
-        return eval_quoted(x, env)
-
-    elif isinstance(x, Symbol):
-        return eval_symbol(x, env)
-
-    elif isinstance(x, List):
-        if len(x) == 0:
-            return x
-        head = x[0]
-        if isinstance(head, Symbol):
-            if head.value() == "begin":
-                return eval_begin(x, env)
-            elif head.value() == "set!":
-                return eval_set(x, env)
-            elif head.value() == "if":
-                return eval_if(x, env)
-            elif head.value() == "lambda":
-                return eval_lambda(x, env)
-            else:
-                return eval_apply_funcall(x, env)
-        else:
-            return eval_apply_funcall(x, env)
-
-    else:
-        raise RuntimeError("Don't know how to evaluate %s" % x)
+    match x:
+        case int(n) | float(n) | str(n): return n
+        case Quoted(): return eval_quoted(x, env)
+        case Symbol(): return eval_symbol(x, env)
+        case []: return x
+        case [first, *_]:
+            match first:
+                case Symbol():
+                    match first.value():
+                        case "begin": return eval_begin(x, env)
+                        case "set!": return eval_set(x, env)
+                        case "if": return eval_if(x, env)
+                        case "lambda": return eval_lambda(x, env)
+                        case _: return eval_apply_funcall(x, env)
+                case _:
+                    return eval_apply_funcall(x, env)
+        case _: raise SchemeInvalidExpressionError(x)
 
 
 def eval_quoted(x, env: SchemeEnvironment):
@@ -110,45 +118,61 @@ def eval_symbol(x, env: SchemeEnvironment):
 
 
 def eval_begin(x, env: SchemeEnvironment):
-    val = None
-    for form in x[1:]:
-        val = eval_scheme(form, env)
-    return val
+    match x:
+        case [_, *forms]:
+            val = None
+            for form in forms:
+                val = eval_scheme(form, env)
+            return val
+        case _:
+            raise SchemeInvalidExpressionError(x)
 
 
 def eval_set(x, env: SchemeEnvironment):
-    assert len(x) == 3
-    var = x[1]
-    val = x[2]
-    env.set_var(var, eval_scheme(val, env))
-    return None
+    match x:
+        case [_, var, val]:
+            env.set_var(var, eval_scheme(val, env))
+            return var.value()
+        case _:
+            raise SchemeInvalidExpressionError(x)
 
 
 def eval_if(x, env: SchemeEnvironment):
-    assert len(x) == 3 or len(x) == 4
-    test_form, true_form = x[1], x[2]
-    false_form = x[3] if len(x) == 4 else []
-    if eval_scheme(test_form, env):
-        return eval_scheme(true_form, env)
-    else:
-        return eval_scheme(false_form, env)
+    match x:
+        case [_, test_form, true_form]:
+            if eval_scheme(test_form, env):
+                return eval_scheme(true_form, env)
+            return
+        case [_, test_form, true_form, false_form]:
+            if eval_scheme(test_form, env):
+                return eval_scheme(true_form, env)
+            else:
+                return eval_scheme(false_form, env)
+        case _:
+            raise SchemeInvalidExpressionError(x)
 
 
 def eval_lambda(x, env: SchemeEnvironment):
-    assert len(x) >= 2
-    params = x[1]
-    body = [Symbol("begin")] + x[2:]
+    match x:
+        case [_, params, *body]:
+            body = [Symbol("begin")] + body
 
-    def _proc(*args):
-        return eval_scheme(body, env.extend(params, args))
+            def _proc(*args):
+                return eval_scheme(body, env.extend(params, args))
 
-    return _proc
+            return _proc
+        case _:
+            raise SchemeInvalidExpressionError(x)
 
 
 def eval_apply_funcall(x, env: SchemeEnvironment):
-    func = eval_scheme(x[0], env)
-    args = [eval_scheme(arg, env) for arg in x[1:]]
-    return func(*args)
+    match x:
+        case [func, *args]:
+            func = eval_scheme(func, env)
+            args = [eval_scheme(arg, env) for arg in args]
+            return func(*args)
+        case _:
+            raise SchemeInvalidExpressionError(x)
 
 
 def scheme():
@@ -159,14 +183,20 @@ def scheme():
 
     while True:
         try:
-            input = session.prompt()
+            input = session.prompt().strip()
+            if not input:
+                continue
+            res = eval_scheme(parse_sexp(input), global_env)
         except KeyboardInterrupt:
             continue
         except EOFError:
             print("Bye bye.")
             exit(0)
+        except Exception as e:
+            print(e)
+            continue
         else:
-            print(eval_scheme(parse_sexp(input), global_env))
+            print(res)
 
 
 if __name__ == "__main__":
